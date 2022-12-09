2a5ccc4b14beec1563901c7806b54ba3c6969aec
s1809585 <s.saxena-1@sms.ed.ac.uk>
2022-12-09 11:05:39 +0000

    Expose print board functionality to main program

--- src/player.h
+++ src/player.h
@@ -12,6 +12,8 @@ class Player {
         std::unordered_map<char, int> d_spotsLeft;
     public:
         void reportResults();
+        void print_ships() {d_ships.printBoard();}
+        void print_targets() {d_targets.printBoard();}
     friend void evaluateTurn(Player& attack, Player& defense);
 };
 
